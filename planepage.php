0f4ae92725a8858392514d1b1adeaab95bb37648
Rremesal <raul.remesal@gmail.com>
2022-03-29 20:13:00 +0200

    header toegevoegd

--- planepage.php
+++ planepage.php
@@ -1,10 +1,25 @@
+<?php include("connection.php"); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Document</title>
+    <title>Vliegtuigdetails</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <?php require("menu.php");?>
+    <div id="detailDiv">
+        <?php
+            $planeId = $_GET['id'];
+            $query = "SELECT * FROM vliegtuigen WHERE vliegtuignummer=$planeId";
+            $stm = $conn->prepare($query);
+            if($stm->execute()) {
+                $data = $stm->fetch(PDO::FETCH_OBJ);
+            ?>  <h2><?php echo $data->type; ?></h2>
+            <?php
+            } 
+            ?>
+    </div>
+      
     
 </body>
 </html>
