a3eafa6e9230af11da139631c06a103d06294c7e
Rremesal <raul.remesal@gmail.com>
2022-03-31 12:15:11 +0200

    planning formulier gemaakt

--- home.php
+++ home.php
@@ -4,12 +4,19 @@
     if(isset($_GET['id'])) {
         $planeId = $_GET['id'];
 
-        $query = "SELECT * FROM vliegtuigen WHERE vliegtuignummer = $planeId";
-        $stm = $conn->prepare($query);
+        $queryPlane = "SELECT * FROM vliegtuigen WHERE vliegtuignummer = $planeId";
+        $stm = $conn->prepare($queryPlane);
         if ($stm->execute()) {
             $data = $stm->fetch(PDO::FETCH_OBJ);
             echo var_dump($data);
-        } else echo "ophalen van data mislukt";
+        } else echo "ophalen van data vliegtuig mislukt";
+
+        $queryPlanning = "SELECT * FROM planning WHERE vliegtuignummer = $planeId";
+        $stm = $conn->prepare($queryPlanning);
+        if($stm->execute()) {
+            $dataPlanning = $stm->fetch(PDO::FETCH_OBJ);
+            echo var_dump($data);
+        } else echo "ophalen van data planning mislukt"; 
     }
 
 ?> 
@@ -27,25 +34,49 @@
         <img src="background.png"/>
         <h1>Welkom</h1>
     </div>
+
+    <div class="flexcontainer">
         
 
     
-    <div id="toevoegDiv">
-        <h2>Vliegtuigen</h2>
-    
-        <form method="POST">
-            <label>Type <input type="text" name="txtType" id="inputType" value="<?php if(isset($data)) echo $data->type; ?>"/></label><br/>
-            <label>Vliegtuigmaatschappij <input type="text" name="txtVliegtuigMa" id="inputVliegtuigMa" value="<?php if(isset($data)) echo $data->vliegmaatschappij; ?>"/></label><br/>
-            <label>Status </label>
-            <select id="statusDropDown" name="selStatus" value="<?php if(isset($data)) echo $data->status; ?>">
-                <option <?php if(isset($data)) if($data->status == "") echo "selected ";?> value="-"></option>
-                <option <?php if(isset($data)) if($data->status == "NOT IN OPERATION") echo "selected ";?>value="NOT IN OPERATION">IN PRODUCTION</option>
-                <option <?php if(isset($data)) if($data->status == "READY FOR USE") echo "selected ";?>value="READY FOR USE">READY FOR USE</option>
-            </select>
-            <br/>
-            <br/>
-            <input type="submit" name="btnSubmit" id="btnSubmit"/>
-        </form>
+        <div id="toevoegDiv">
+            <h2>Vliegtuigen</h2>
+        
+            <form method="POST">
+                <label>Type <input type="text" name="txtType" id="inputType" value="<?php if(isset($data)) echo $data->type; ?>"/></label><br/>
+                <label>Vliegtuigmaatschappij <input type="text" name="txtVliegtuigMa" id="inputVliegtuigMa" value="<?php if(isset($data)) echo $data->vliegmaatschappij; ?>"/></label><br/>
+                <label>Status </label>
+                <select id="statusDropDown" name="selStatus" value="<?php if(isset($data)) echo $data->status; ?>">
+                    <option <?php if(isset($data)) if($data->status == "") echo "selected ";?> value="-"></option>
+                    <option <?php if(isset($data)) if($data->status == "NOT IN OPERATION") echo "selected ";?>value="NOT IN OPERATION">IN PRODUCTION</option>
+                    <option <?php if(isset($data)) if($data->status == "READY FOR USE") echo "selected ";?>value="READY FOR USE">READY FOR USE</option>
+                </select>
+                <br/>
+                <br/>
+                <input type="submit" name="btnSubmit" id="btnSubmit"/>
+            </form>
+        </div>
+
+        <div id="toevoegDivPlanning">
+            <h2>Planning</h2>
+        
+            <form method="POST">
+                <label>Bestemming <input type="text" name="txtDestination" id="inputDestination" value="<?php if(isset($data)) echo $dataPlanning->bestemming; ?>"/></label><br/>
+                <label>Vertrekdatum <input type="date" name="dateDepartureDate" id="inputDepartureDate" value="<?php if(isset($data)) echo $data->vertrekdatum; ?>"/></label><br/>
+                <label>Retourdatum <input type="date" name="dateRetourDate" id="inputRetourDate" value="<?php if(isset($data)) echo $data->retourdatum; ?>"/></label><br/>
+                <label>Status </label>
+                <select id="statusDropDownPlanning" name="selStatus" value="<?php if(isset($data)) echo $dataPlanning->status; ?>">
+                    <option <?php if(isset($data)) if($data->status == "") echo "selected ";?> value="-"></option>
+                    <option <?php if(isset($data)) if($data->status == "READY FOR TAKEOFF") echo "selected ";?>value="READY FOR TAKEOFF">READY FOR TAKEOFF</option>
+                    <option <?php if(isset($data)) if($data->status == "DEPARTED") echo "selected ";?>value="DEPARTED">DEPARTED</option>
+                    <option <?php if(isset($data)) if($data->status == "CANCELLED") echo "selected ";?>value="CANCELLED">CANCELLED</option>
+                    <option <?php if(isset($data)) if($data->status == "DELAYED") echo "selected ";?>value="DELAYED">DELAYED</option>
+                </select>
+                <br/>
+                <br/>
+                <input type="submit" name="btnSubmit" id="btnSubmitPlanning"/>
+            </form>
+        </div>
     </div>
 
     <?php 
