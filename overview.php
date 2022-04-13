ff1a5331769f523fcc00c169740cd1fb0506b409
Rremesal <raul.remesal@gmail.com>
2022-04-13 11:58:45 +0200

    zoekfunctie voor actieve vliegtuigen toegevoegd

--- overview.php
+++ overview.php
@@ -9,7 +9,9 @@
     <?php require("menu.php");?>
     <div id="bodyDiv">
         <form class="tools" method="POST">
-            <label><input type="radio" name="radioActive"/>alleen actieve vliegtuigen</label>
+            <label><input type="radio" value="actieve vliegtuigen" name="radioActive"/>alleen actieve vliegtuigen</label><br/>
+            <label><input type="radio" value="alles tonen" name="radioActive"/>alles tonen</label><br/>
+            <br/>
             <input type="submit" name="btnSubmit"/>
         </form>
     
@@ -21,21 +23,54 @@
 
 
             <?php 
-                $query = "SELECT * FROM vliegtuigen";
-                $stm = $conn->prepare($query);
-                if($stm->execute()) {
-                    while ($rows=$stm->fetch(PDO::FETCH_ASSOC)) {
-                        echo "<tr>". 
-                        "<td>".$rows['type']."</td>".
-                        "<td>".$rows['vliegmaatschappij']."</td>".
-                        "<td>".$rows['status']."</td>".
-                        "<td>"."<a href='home.php?id=".$rows['vliegtuignummer']."'>wijzig</a></td>".
-                        "<td>"."<a href='planepage.php?id=".$rows['vliegtuignummer']."'>details</a></td>".
-                        "<td>"."<a href='planning.php?id=".$rows['vliegtuignummer']."'>planning</a>".
-                        "</tr>";
-                    }
-                } else echo "ophalen van vliegtuigdata mislukt";
+                
 
+                if(isset($_POST['btnSubmit']) && $_POST['radioActive'] == "actieve vliegtuigen" ) {
+                    $query = "SELECT * FROM vliegtuigen WHERE status='ACTIVE'";
+                    $stm = $conn->prepare($query);
+                    if($stm->execute()) {
+                        while ($rows=$stm->fetch(PDO::FETCH_ASSOC)) {
+                            echo "<tr>". 
+                            "<td>".$rows['type']."</td>".
+                            "<td>".$rows['vliegmaatschappij']."</td>".
+                            "<td>".$rows['status']."</td>".
+                            "<td>"."<a href='home.php?id=".$rows['vliegtuignummer']."'>wijzig</a></td>".
+                            "<td>"."<a href='planepage.php?id=".$rows['vliegtuignummer']."'>details</a></td>".
+                            "<td>"."<a href='planning.php?id=".$rows['vliegtuignummer']."'>planning</a>".
+                            "</tr>";
+                        }
+                    } else echo "ophalen van vliegtuigdata mislukt";
+                } else if(isset($_POST['btnSubmit']) && $_POST['radioActive'] == "alles tonen" ) {
+                        $query = "SELECT * FROM vliegtuigen";
+                        $stm = $conn->prepare($query);
+                        if($stm->execute()) {
+                            while ($rows=$stm->fetch(PDO::FETCH_ASSOC)) {
+                                echo "<tr>". 
+                                "<td>".$rows['type']."</td>".
+                                "<td>".$rows['vliegmaatschappij']."</td>".
+                                "<td>".$rows['status']."</td>".
+                                "<td>"."<a href='home.php?id=".$rows['vliegtuignummer']."'>wijzig</a></td>".
+                                "<td>"."<a href='planepage.php?id=".$rows['vliegtuignummer']."'>details</a></td>".
+                                "<td>"."<a href='planning.php?id=".$rows['vliegtuignummer']."'>planning</a>".
+                                "</tr>";
+                            }
+                        } else echo "ophalen van vliegtuigdata mislukt";
+                } else {
+                    $query = "SELECT * FROM vliegtuigen";
+                        $stm = $conn->prepare($query);
+                        if($stm->execute()) {
+                            while ($rows=$stm->fetch(PDO::FETCH_ASSOC)) {
+                                echo "<tr>". 
+                                "<td>".$rows['type']."</td>".
+                                "<td>".$rows['vliegmaatschappij']."</td>".
+                                "<td>".$rows['status']."</td>".
+                                "<td>"."<a href='home.php?id=".$rows['vliegtuignummer']."'>wijzig</a></td>".
+                                "<td>"."<a href='planepage.php?id=".$rows['vliegtuignummer']."'>details</a></td>".
+                                "<td>"."<a href='planning.php?id=".$rows['vliegtuignummer']."'>planning</a>".
+                                "</tr>";
+                            }
+                        } else echo "ophalen van vliegtuigdata mislukt";
+                }
                 
 
             ?>
